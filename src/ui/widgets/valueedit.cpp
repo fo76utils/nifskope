12c3ef62b0f77946c04a3f71743430fce1bc90b8
fo76utils <87907510+fo76utils@users.noreply.github.com>
2024-07-24 14:13:29 +0200

    Set Euler rotation labels correctly as X = Roll, Y = Pitch, Z = Yaw

--- src/ui/widgets/valueedit.cpp
+++ src/ui/widgets/valueedit.cpp
@@ -798,7 +798,7 @@ void RotationEdit::setupMode()
 	case mEuler:
 		{
 			actMode->setText( "Euler" );
-			QStringList labs{ "X", "Y", "Z" };
+			QStringList labs{ "R", "P", "Y" };
 
 			for ( int x = 0; x < 4; x++ ) {
 				l[x]->setText( labs.value( x ) );
