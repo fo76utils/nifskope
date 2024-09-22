b2e1a4a39a9c76bc0c0c43f08dcd9a9d4abb7733
fo76utils <87907510+fo76utils@users.noreply.github.com>
2024-09-22 23:41:22 +0200

    Implementing support for saving edited Starfield materials

--- src/spells/sfmatexport.cpp
+++ src/spells/sfmatexport.cpp
@@ -10,6 +10,10 @@
 #include <QDir>
 #include <QFile>
 #include <QFileDialog>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
 #include <QSettings>
 
 //! Export Starfield material as JSON format .mat file
@@ -259,11 +263,36 @@ public:
 		return spStarfieldMaterialExport::canExportMaterial( nif, index );
 	}
 
+	static QString getOutputFileName( const NifModel * nif, const std::string_view & matFilePath );
 	static std::string getBaseName( const std::string_view & fullPath );
 	static void renameMaterial(
 		std::string & matFileData, const std::string_view & matPath, const std::string_view & newPath );
 	QModelIndex cast( NifModel * nif, const QModelIndex & index ) override final;
 };
+
+QString spStarfieldMaterialSaveAs::getOutputFileName( const NifModel * nif, const std::string_view & matFilePath )
+{
+	QString	dirName = nif->getFolder();
+	bool	setLastFilePath = false;
+	if ( dirName.isEmpty() || dirName.contains( ".ba2/", Qt::CaseInsensitive ) || dirName.contains( ".bsa/", Qt::CaseInsensitive ) ) {
+		QSettings	settings;
+		dirName = settings.value( "Spells//Extract File/Last File Path", QString() ).toString();
+		setLastFilePath = true;
+	}
+	if ( !dirName.isEmpty() && !dirName.endsWith( '/' ) )
+		dirName.append( QChar('/') );
+	dirName.append( QString::fromStdString( getBaseName( matFilePath ) ) );
+	QString	fileName = QFileDialog::getSaveFileName( qApp->activeWindow(), tr("Choose a .mat file for export"), dirName, "Material (*.mat)" );
+	if ( fileName.isEmpty() )
+		return QString();
+	if ( !fileName.endsWith( ".mat", Qt::CaseInsensitive ) )
+		fileName.append( ".mat" );
+	if ( setLastFilePath ) {
+		QSettings	settings;
+		settings.setValue( "Spells//Extract File/Last File Path", QFileInfo( fileName ).dir().absolutePath() );
+	}
+	return fileName;
+}
 
 std::string spStarfieldMaterialSaveAs::getBaseName( const std::string_view & fullPath )
 {
@@ -352,19 +381,9 @@ QModelIndex spStarfieldMaterialSaveAs::cast( NifModel * nif, const QModelIndex &
 			if ( matFileData.empty() )
 				return index;
 
-			QString	dirName = nif->getFolder();
-			if ( dirName.isEmpty() || dirName.contains( ".ba2/", Qt::CaseInsensitive ) || dirName.contains( ".bsa/", Qt::CaseInsensitive ) ) {
-				QSettings	settings;
-				dirName = settings.value( "Spells//Extract File/Last File Path", QString() ).toString();
-			}
-			if ( !dirName.isEmpty() && !dirName.endsWith( '/' ) )
-				dirName.append( QChar('/') );
-			dirName.append( QString::fromStdString( getBaseName( matFilePath ) ) );
-			QString	fileName = QFileDialog::getSaveFileName( qApp->activeWindow(), tr("Choose a .mat file for export"), dirName, "Material (*.mat)" );
+			QString	fileName = getOutputFileName( nif, matFilePath );
 			if ( fileName.isEmpty() )
 				return index;
-			if ( !fileName.endsWith( ".mat", Qt::CaseInsensitive ) )
-				fileName.append( ".mat" );
 
 			spStarfieldMaterialExport::generateResourceIDs( matFileData, materials );
 			matFileData += '\n';
@@ -384,6 +403,296 @@ QModelIndex spStarfieldMaterialSaveAs::cast( NifModel * nif, const QModelIndex &
 }
 
 REGISTER_SPELL( spStarfieldMaterialSaveAs )
+
+class CE2MaterialToJSON
+{
+protected:
+	struct MatObject
+	{
+		QJsonObject	jsonObject;
+		const CE2MaterialObject *	o;
+		BSMaterialsCDB::BSResourceID	objectID;
+		QString getResourceID() const;
+	};
+	QMap< const CE2MaterialObject *, MatObject >	matObjectMap;
+	CE2MaterialDB *	materials;
+	QString	matName;
+	unsigned char	objTypeIndex[8];
+	QVector< MatObject * >	matObjects;
+
+	MatObject * getObject( const CE2MaterialObject * o );
+	void createObject( const CE2MaterialObject * o );
+	static void createComponent( QJsonArray & components, const QJsonObject & data, const char * type, int n = 0 );
+	static QJsonObject createColor( FloatVector4 c );
+	void createLink( QJsonArray & components, const CE2MaterialObject * o, int n = 0 );
+	void createLayeredMaterial( QJsonArray & components, const CE2Material * o );
+	void createBlender( QJsonArray & components, const CE2Material::Blender * o );
+	void createLayer( QJsonArray & components, const CE2Material::Layer * o );
+	void createMaterial( QJsonArray & components, const CE2Material::Material * o );
+	void createTextureSet( QJsonArray & components, const CE2Material::TextureSet * o );
+	void createUVStream( QJsonArray & components, const CE2Material::UVStream * o );
+public:
+	CE2MaterialToJSON( NifModel * nif, const QString & matFilePath, const CE2MaterialObject * o );
+	~CE2MaterialToJSON();
+	QByteArray getJSONData() const;
+};
+
+QString CE2MaterialToJSON::MatObject::getResourceID() const
+{
+	if ( !o )
+		return QString();
+	if ( o->type == 1 && !o->parent )
+		return "<this>";
+
+	char	tmpBuf[64];
+	int	len = std::snprintf( tmpBuf, 64, "res:%08X:%08X:%08X",
+							(unsigned int) objectID.dir, (unsigned int) objectID.file, (unsigned int) objectID.ext );
+	return QString::fromLatin1( tmpBuf, len );
+}
+
+CE2MaterialToJSON::MatObject * CE2MaterialToJSON::getObject( const CE2MaterialObject * o )
+{
+	if ( !o )
+		return nullptr;
+
+	auto	i = matObjectMap.find( o );
+	if ( i == matObjectMap.end() ) {
+		MatObject	m;
+		m.o = o;
+		do {
+			m.objectID = spStarfieldMaterialExport::generateResourceID( nullptr, nullptr, materials );
+			for ( auto j = matObjectMap.begin(); j != matObjectMap.end(); j++ ) {
+				if ( j.value().objectID == m.objectID ) {
+					m.objectID = BSMaterialsCDB::BSResourceID( 0, 0, 0 );
+					break;
+				}
+			}
+		} while ( !m.objectID );
+		i = matObjectMap.insert( o, m );
+		matObjects.append( &( i.value() ) );
+		createObject( o );
+	}
+	return &( i.value() );
+}
+
+void CE2MaterialToJSON::createObject( const CE2MaterialObject * o )
+{
+	if ( !( o && o->type >= 1 && o->type <= 6 ) )
+		return;
+
+	static const char *	baseObjectPaths[6] = {
+		"materials\\layered\\root\\layeredmaterials.mat",
+		"materials\\layered\\root\\blenders.mat",
+		"materials\\layered\\root\\layers.mat",
+		"materials\\layered\\root\\materials.mat",
+		"materials\\layered\\root\\texturesets.mat",
+		"materials\\layered\\root\\uvstreams.mat"
+	};
+
+	MatObject &	m = matObjectMap.find( o ).value();
+	QJsonObject &	jsonObject = m.jsonObject;
+	jsonObject.insert( "Parent", baseObjectPaths[o->type - 1] );
+	if ( !( o->type == 1 && !o->parent ) )
+		jsonObject.insert( "ID", m.getResourceID() );
+	if ( o->parent ) {
+		QJsonArray	edges;
+		QJsonObject	outerEdge;
+		outerEdge.insert( "EdgeIndex", 0 );
+		outerEdge.insert( "To", getObject( o->parent )->getResourceID() );
+		outerEdge.insert( "Type", "BSComponentDB2::OuterEdge" );
+		edges.append( outerEdge );
+		jsonObject.insert( "Edges", edges );
+	}
+	if ( o->type != 1 || o->parent )
+		objTypeIndex[o->type - 1] = objTypeIndex[o->type - 1] + 1;
+
+	QJsonArray	components;
+	{
+		static const char *	objNames[6] = { "LOD", "Blender", "Layer", "Material", "TextureSet", "UVStream" };
+		QJsonObject	ctNameData;
+		QString	name;
+		if ( o->type == 1 && !o->parent )
+			name = matName;
+		else if ( o->name && o->name[0] )
+			name = QString::fromUtf8( o->name, -1 );
+		else
+			name = matName + QString( "_%1%2" ).arg( objNames[o->type - 1] ).arg( objTypeIndex[o->type - 1] );
+		ctNameData.insert( "Name", name );
+		createComponent( components, ctNameData, "BSComponentDB::CTName" );
+	}
+	if ( o->type == 1 )
+		createLayeredMaterial( components, static_cast< const CE2Material * >( o ) );
+	else if ( o->type == 2 )
+		createBlender( components, static_cast< const CE2Material::Blender * >( o ) );
+	else if ( o->type == 3 )
+		createLayer( components, static_cast< const CE2Material::Layer * >( o ) );
+	else if ( o->type == 4 )
+		createMaterial( components, static_cast< const CE2Material::Material * >( o ) );
+	else if ( o->type == 5 )
+		createTextureSet( components, static_cast< const CE2Material::TextureSet * >( o ) );
+	else if ( o->type == 6 )
+		createUVStream( components, static_cast< const CE2Material::UVStream * >( o ) );
+	jsonObject.insert( "Components", components );
+}
+
+void CE2MaterialToJSON::createComponent( QJsonArray & components, const QJsonObject & data, const char * type, int n )
+{
+	QJsonObject	componentObject;
+	componentObject.insert( "Data", data );
+	componentObject.insert( "Index", n );
+	componentObject.insert( "Type", type );
+	components.append( componentObject );
+}
+
+QJsonObject CE2MaterialToJSON::createColor( FloatVector4 c )
+{
+	QJsonObject	xmfloatData;
+	xmfloatData.insert( "x", QString::number( c[0] ) );
+	xmfloatData.insert( "y", QString::number( c[1] ) );
+	xmfloatData.insert( "z", QString::number( c[2] ) );
+	xmfloatData.insert( "w", QString::number( c[3] ) );
+	QJsonObject	xmfloatValue;
+	xmfloatValue.insert( "Data", xmfloatData );
+	xmfloatValue.insert( "Type", "XMFLOAT4" );
+
+	QJsonObject	colorData;
+	colorData.insert( "Value", xmfloatValue );
+	QJsonObject	colorObject;
+	colorObject.insert( "Data", colorData );
+	colorObject.insert( "Type", "BSMaterial::Color" );
+
+	return colorObject;
+}
+
+void CE2MaterialToJSON::createLink( QJsonArray & components, const CE2MaterialObject * o, int n )
+{
+	if ( !( o && o->type >= 1 && o->type <= 6 ) )
+		return;
+	QJsonObject	linkData;
+	linkData.insert( "ID", getObject( o )->getResourceID() );
+	const char *	t = "BSMaterial::LODMaterialID";
+	if ( o->type == 2 )
+		t = "BSMaterial::BlenderID";
+	else if ( o->type == 3 )
+		t = "BSMaterial::LayerID";
+	else if ( o->type == 4 )
+		t = "BSMaterial::MaterialID";
+	else if ( o->type == 5 )
+		t = "BSMaterial::TextureSetID";
+	else if ( o->type == 6 )
+		t = "BSMaterial::UVStreamID";
+	createComponent( components, linkData, t, n );
+}
+
+#define getCE2MatString( t, n ) t[std::min< unsigned int >( n, ( sizeof( t ) / sizeof( char * ) ) - 1U )]
+
+void CE2MaterialToJSON::createLayeredMaterial( QJsonArray & components, const CE2Material * o )
+{
+	for ( int i = 0; i < CE2Material::maxLayers; i++ ) {
+		if ( !( ( o->layerMask & ( 1U << i ) ) && o->layers[i] ) )
+			continue;
+
+		createLink( components, o->layers[i], i );
+
+		if ( i > 0 && i <= CE2Material::maxBlenders && o->blenders[i - 1] )
+			createLink( components, o->blenders[i - 1], i - 1 );
+	}
+
+	for ( int i = 0; i < CE2Material::maxLODMaterials; i++ ) {
+		if ( o->lodMaterials[i] )
+			createLink( components, o->lodMaterials[i], i );
+	}
+
+	{
+		QJsonObject	shaderRoute;
+		shaderRoute.insert( "Route", getCE2MatString( CE2Material::shaderRouteNames, o->shaderRoute ) );
+		createComponent( components, shaderRoute, "BSMaterial::ShaderRouteComponent" );
+	}
+	{
+		QJsonObject	shaderModel;
+		shaderModel.insert( "FileName", getCE2MatString( CE2Material::shaderModelNames, o->shaderModel ) );
+		createComponent( components, shaderModel, "BSMaterial::ShaderModelComponent" );
+	}
+	if ( o->flags & CE2Material::Flag_TwoSided ) {
+		QJsonObject	paramBool;
+		paramBool.insert( "Value", "true" );
+		createComponent( components, paramBool, "BSMaterial::ParamBool" );
+	}
+	// TODO
+}
+
+void CE2MaterialToJSON::createBlender( QJsonArray & components, const CE2Material::Blender * o )
+{
+	if ( o->uvStream )
+		createLink( components, o->uvStream );
+	// TODO
+}
+
+void CE2MaterialToJSON::createLayer( QJsonArray & components, const CE2Material::Layer * o )
+{
+	if ( o->material )
+		createLink( components, o->material );
+	if ( o->uvStream )
+		createLink( components, o->uvStream );
+}
+
+void CE2MaterialToJSON::createMaterial( QJsonArray & components, const CE2Material::Material * o )
+{
+	if ( o->textureSet )
+		createLink( components, o->textureSet );
+	// TODO
+}
+
+void CE2MaterialToJSON::createTextureSet( QJsonArray & components, const CE2Material::TextureSet * o )
+{
+	for ( int i = 0; i < CE2Material::TextureSet::maxTexturePaths; i++ ) {
+		if ( ( o->texturePathMask & ( 1U << i ) ) && o->texturePaths[i] && !o->texturePaths[i]->empty() ) {
+			QJsonObject	textureFile;
+			textureFile.insert( "FileName",
+								QString::fromUtf8( o->texturePaths[i]->data(), o->texturePaths[i]->length() ) );
+			createComponent( components, textureFile, "BSMaterial::MRTextureFile", i );
+		}
+		if ( o->textureReplacementMask & ( 1U << i ) ) {
+			QJsonObject	textureReplacement;
+			textureReplacement.insert( "Enabled", "true" );
+			textureReplacement.insert( "Color", createColor( FloatVector4( o->textureReplacements[i] ) / 255.0f ) );
+			createComponent( components, textureReplacement, "BSMaterial::TextureReplacement", i );
+		}
+	}
+	// TODO
+}
+
+void CE2MaterialToJSON::createUVStream( QJsonArray & components, const CE2Material::UVStream * o )
+{
+	// TODO
+}
+
+CE2MaterialToJSON::CE2MaterialToJSON( NifModel * nif, const QString & matFilePath, const CE2MaterialObject * o )
+  : materials( nif->getCE2Materials() ),
+	matName( QString::fromStdString( spStarfieldMaterialSaveAs::getBaseName( matFilePath.toStdString() ) ) )
+{
+	for ( int i = 0; i < 8; i++ )
+		objTypeIndex[i] = 0;
+	(void) getObject( o );
+}
+
+CE2MaterialToJSON::~CE2MaterialToJSON()
+{
+}
+
+QByteArray CE2MaterialToJSON::getJSONData() const
+{
+	QJsonObject	matFileObject;
+	matFileObject.insert( "Version", 1 );
+
+	QJsonArray	jsonObjects;
+	for ( auto m : matObjects )
+		jsonObjects.append( m->jsonObject );
+	matFileObject.insert( "Objects", jsonObjects );
+
+	QJsonDocument	matFileData( matFileObject );
+	return matFileData.toJson( QJsonDocument::Indented );
+}
 
 //! Save edited Starfield material as JSON format .mat file
 class spStarfieldEditedMaterialSaveAs final : public Spell
@@ -408,8 +717,37 @@ public:
 
 QModelIndex spStarfieldEditedMaterialSaveAs::cast( NifModel * nif, const QModelIndex & index )
 {
-	// TODO: implement this
-	(void) nif;
+	QModelIndex	idx = nif->getBlockIndex( index );
+	if ( nif->blockInherits( idx, "BSGeometry" ) )
+		idx = nif->getBlockIndex( nif->getLink( idx, "Shader Property" ) );
+	QString	matFilePath;
+	if ( nif->blockInherits( idx, "BSShaderProperty" ) ) {
+		matFilePath = nif->get<QString>( idx, "Name" );
+		idx = nif->getIndex( idx, "Material" );
+	} else {
+		return index;
+	}
+	if ( !idx.isValid() || matFilePath.isEmpty() )
+		return index;
+
+	AllocBuffers	matBuf;
+	const CE2Material *	mat = reinterpret_cast< const CE2Material * >( nif->updateSFMaterial( matBuf, idx ) );
+	if ( !mat )
+		return index;
+
+	matFilePath = spStarfieldMaterialSaveAs::getOutputFileName(
+						nif, Game::GameManager::get_full_path( matFilePath, "materials/", ".mat" ) );
+	if ( matFilePath.isEmpty() )
+		return index;
+
+	CE2MaterialToJSON	matConv( nif, matFilePath, mat );
+
+	QFile	matFile( matFilePath );
+	if ( matFile.open( QIODevice::WriteOnly ) )
+		matFile.write( matConv.getJSONData() );
+	else
+		QMessageBox::critical( nullptr, "NifSkope error", QString( "Could not open output file" ) );
+
 	return index;
 }
 
