76dc19cb0385106654f9cb0514abcabcb67b55a8
fo76utils <87907510+fo76utils@users.noreply.github.com>
2024-10-16 21:24:54 +0200

    Limit the number of layers to 5 if the shader model is Skin5Layer

--- src/gl/renderer.cpp
+++ src/gl/renderer.cpp
@@ -1220,15 +1220,12 @@ bool Renderer::setupProgramCE2( const NifModel * nif, Program * prog, Shape * me
 	// material layers
 	int	texUniforms[9];
 	FloatVector4	replUniforms[9];
-	// limit the number of layers to 6, or 2 if the shader model is Eye1Layer
-	int	numLayers = std::countr_one( mat->layerMask & ( mat->shaderModel != 41 ? 0x3FU : 0x03U ) );
+	// limit the number of layers to 6, or 2 if the shader model is Eye1Layer, or 5 for Skin5Layer
+	int	numLayers = std::countr_one( mat->layerMask & ( mat->shaderModel != 41 ?
+														( mat->shaderModel != 48 ? 0x3FU : 0x1FU ) : 0x03U ) );
 	prog->uni1i( "lm.numLayers", numLayers );
 	for ( int i = 0; i < numLayers; i++ ) {
 		const CE2Material::Layer *	layer = mat->layers[i];
-		for ( int j = 0; j < 9; j++ ) {
-			texUniforms[j] = 0;
-			replUniforms[j] = FloatVector4( 0.0f );
-		}
 		std::uint32_t	textureSlotMap = 0;
 		std::uint32_t	textureReplModes = 0x0055955E;	// 2, 3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1
 		const CE2Material::Blender *	blender = nullptr;
@@ -1251,7 +1248,7 @@ bool Renderer::setupProgramCE2( const NifModel * nif, Program * prog, Shape * me
 		if ( !textureSet ) [[unlikely]]
 			textureSet = &CE2Material::defaultTextureSet;
 		prog->uni1f_l( prog->uniLocation("lm.layers[%d].material.textureSet.floatParam", i), textureSet->floatParam );
-		for ( int j = 0; j < 9 && j < CE2Material::TextureSet::maxTexturePaths; j++ ) {
+		for ( int j = 0; j < 9; j++ ) {
 			int	k = j + int( textureSlotMap & 15U );
 			const std::string_view *	texturePath = textureSet->texturePaths[k];
 			std::uint32_t	textureReplacement = textureSet->textureReplacements[k];
@@ -1276,6 +1273,7 @@ bool Renderer::setupProgramCE2( const NifModel * nif, Program * prog, Shape * me
 			} else if ( j == 2 && ( mat->flags & CE2Material::Flag_HasOpacity ) && i == mat->alphaSourceLayer ) {
 				uvStream = mat->alphaUVStream;
 			}
+			replUniforms[j] = FloatVector4( 0.0f );
 			texUniforms[j] = lsp->getSFTexture( texunit, replUniforms[j], *texturePath, textureReplacement, textureReplacementMode, uvStream );
 		}
 		if ( blendMode == 4 ) [[unlikely]] {
