632e38916e9f2f0e6d46c410df4e474845c2bc32
fo76utils <87907510+fo76utils@users.noreply.github.com>
2025-04-11 14:52:58 +0200

    spSetAllColor: implemented for BSTriShape and BSGeometry

--- src/spells/color.cpp
+++ src/spells/color.cpp
@@ -57,18 +57,52 @@ public:
 
 	bool isApplicable( const NifModel * nif, const QModelIndex & index ) override final
 	{
-		return nif->isArray( index ) && nif->getValue( nif->getIndex( index, 0 ) ).isColor();
+		if ( !nif->isArray( index ) )
+			return false;
+		const NifItem * i = nif->getItem( nif->getIndex( index, 0 ), false );
+		if ( !i )
+			return false;
+		if ( i->isColor() )
+			return true;
+		if ( !( i->hasStrType( "BSVertexData" ) || i->hasStrType( "BSVertexDataSSE" ) ) )
+			return false;
+		return nif->getIndex( i, "Vertex Colors" ).isValid();
 	}
 
 	QModelIndex cast( NifModel * nif, const QModelIndex & index ) override final
 	{
-		QModelIndex colorIdx = (nif->isArray( index )) ? nif->getIndex( index, 0 ) : index;
+		NifItem * colorItem = nif->getItem( ( nif->isArray( index ) ? nif->getIndex( index, 0 ) : index ), false );
+		if ( !colorItem )
+			return index;
 
-		auto typ = nif->getValue( colorIdx ).type();
-		if ( typ == NifValue::tColor3 )
-			nif->fillArray<Color3>( index, ColorWheel::choose( nif->get<Color3>( colorIdx ) ) );
-		else if ( typ == NifValue::tColor4 )
-			nif->fillArray<Color4>( index, ColorWheel::choose( nif->get<Color4>( colorIdx ) ) );
+		if ( colorItem->isColor() ) {
+			auto typ = colorItem->valueType();
+			switch ( typ ) {
+			case NifValue::tColor3:
+				nif->fillArray<Color3>( index, ColorWheel::choose( nif->get<Color3>( colorItem ) ) );
+				break;
+			case NifValue::tColor4:
+				nif->fillArray<Color4>( index, ColorWheel::choose( nif->get<Color4>( colorItem ) ) );
+				break;
+			case NifValue::tByteColor4BGRA:
+				nif->fillArray<ByteColor4BGRA>( index,
+												FloatVector4( ColorWheel::choose( nif->get<Color4>( colorItem ) ) ) );
+				break;
+			default:
+				break;
+			}
+		} else if ( QModelIndex colorIdx = nif->getIndex( colorItem, "Vertex Colors" ); colorIdx.isValid() ) {
+			FloatVector4 c = FloatVector4( ColorWheel::choose( nif->get<Color4>( colorIdx ) ) );
+			nif->setState( BaseModel::Processing );
+			int n = nif->rowCount( index );
+			for ( int i = 0; i < n; i++ ) {
+				if ( auto v = nif->getIndex( index, i ); v.isValid() ) {
+					if ( colorIdx = nif->getIndex( v, "Vertex Colors" ); colorIdx.isValid() )
+						nif->set<ByteColor4>( colorIdx, c );
+				}
+			}
+			nif->restoreState();
+		}
 
 		return index;
 	}
